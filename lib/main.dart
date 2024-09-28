794e419feef0fc914089bb9e641500db5ea8ceb7
malika2470 <malika2470@gmail.com>
2024-09-28 11:00:49 -0400

    frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
 
+import 'package:flutter/material.dart';
+//import 'pages/login_page.dart';
+import 'scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: LoginPage(),
+      home: Scanner()
+    );
+  }
+}
